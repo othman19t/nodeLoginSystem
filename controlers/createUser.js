af3b3490051d7d8543755f616cf4d10fbe4e1c50
othman19t <othman19t@gmail.com>
2020-06-01 21:56:53 -0700

    fixing security bug when creating a user

--- controlers/createUser.js
+++ controlers/createUser.js
@@ -2,7 +2,12 @@ const User = require("./../models/users");
 const catchAsync = require("./../errors/catchAsync");
 
 module.exports = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    password_confirm: req.body.password_confirm,
+  });
   res.status(201).json({
     status: "success",
     data: {
