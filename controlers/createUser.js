965364a3f5db0a69b5b7c1e6b1e56759445cfeec
othman19t <othman19t@gmail.com>
2020-06-01 22:08:18 -0700

    creating token and sending it to the user when they create a user

--- controlers/createUser.js
+++ controlers/createUser.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const User = require("./../models/users");
 const catchAsync = require("./../errors/catchAsync");
 
@@ -8,8 +9,17 @@ module.exports = catchAsync(async (req, res, next) => {
     password: req.body.password,
     password_confirm: req.body.password_confirm,
   });
+
+  // log the user in as soon as they signUp
+  const token = jwt.sign(
+    { id: newUser._id },
+    "this-is-my-secret-this-should-be-32-characters-long",
+    { expiresIn: "90d" }
+  );
+
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
